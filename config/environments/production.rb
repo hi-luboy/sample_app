41d4519c7c678fa88667dc737d16fae80f4b808b
jermy <lurenjie@tju.edu.cn>
2018-01-14 14:22:56 +0800

    static assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
-Rails.application.configure do
+SampleApp.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.server_static_assets = true
   # Code is not reloaded between requests.
   config.cache_classes = true
 
